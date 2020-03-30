951b19f72ab66ec7a1244c6e5934db68c79c532a
juditlehoczki <lehoczki.judit@gmail.com>
2020-03-30 11:07:33 +0100

    Twitter added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-linkedin"></i>
+          <i class="fab fa-linkedin-in"></i>
         </a>
         <a
           href="https://dev.to/juditlehoczki"
@@ -25,6 +25,13 @@ const Footer = () => {
           rel="noopener noreferrer"
         >
           <i className="fab fa-github"></i>
+        </a>
+        <a
+          href="https://twitter.com/juditlehoczki"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-twitter"></i>
         </a>
       </address>
     </S.Footer>
