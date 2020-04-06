0688eada3d27c69992bbbb8453a1aa95b8bbc6dc
juditlehoczki <lehoczki.judit@gmail.com>
2020-04-06 20:07:48 +0100

    Updates

---
+++ precache-manifest.e563ae568d320214f5f6e2d2e79c2d6b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ec881a784d5354b4eb27b5bff96a86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0133622762369a1370dc",
+    "url": "/static/css/main.699f7c4a.chunk.css"
+  },
+  {
+    "revision": "97f8425da781b334901f",
+    "url": "/static/js/2.4d2d093d.chunk.js"
+  },
+  {
+    "revision": "da11c87329f26aae43358af958f53a8a",
+    "url": "/static/js/2.4d2d093d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0133622762369a1370dc",
+    "url": "/static/js/main.f9906995.chunk.js"
+  },
+  {
+    "revision": "556d38ca8889f62302bb",
+    "url": "/static/js/runtime-main.5cc4d8a6.js"
+  },
+  {
+    "revision": "d74dca2439c5a496b2c13d7b8631a2df",
+    "url": "/static/media/hangman.d74dca24.png"
+  },
+  {
+    "revision": "6d5f15aaf79cce1c88a081ee91c82847",
+    "url": "/static/media/logo.6d5f15aa.png"
+  },
+  {
+    "revision": "f2c8874e77a11f67ab563f517f09c355",
+    "url": "/static/media/news-backend.f2c8874e.png"
+  },
+  {
+    "revision": "d060865af3e4313d2ed49859a49025d2",
+    "url": "/static/media/news-frontend.d060865a.png"
+  },
+  {
+    "revision": "6ec90ab1382c0bbdc24db0cd55f055c4",
+    "url": "/static/media/portfolio.6ec90ab1.png"
+  },
+  {
+    "revision": "8a81b49e465255a3e5382f8d3ee6e149",
+    "url": "/static/media/profile.8a81b49e.png"
+  },
+  {
+    "revision": "3a4c6b4994114529282e990e351236a7",
+    "url": "/static/media/reuben-alldridge.3a4c6b49.png"
+  }
+]);
